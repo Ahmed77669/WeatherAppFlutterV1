53ed83b378a0fd8d00fe4577f8e4bb6faf68d3dd
unknown <ahmedyousefk6@gmail.com>
2024-08-09 00:27:50 +0300

    first commit

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,16 +1,4 @@
 // globals.dart
 library my_app.globals;
 
-import 'package:shared_preferences/shared_preferences.dart';
-
 String userInput = 'alexandria';
-
-Future<void> saveUserInput(String value) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  prefs.setString('userInput', value);
-}
-
-Future<String?> getUserInput() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getString('userInput');
-}
